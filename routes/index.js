d3bb14f1390669739b682dbe4d8507db1f81719e
Andrei-Serban Zaha <andrei.serbanzaha@snyk.io>
2024-10-23 16:38:21 +0300

    commit

--- routes/index.js
+++ routes/index.js
@@ -162,9 +162,23 @@ exports.updateErr = function(req, res, next) {
       if(err) return next(err);
 
       res.redirect('/');
-    });
+    })
   });
 };
+
+exports.updateErr2 = function(req, res, next) {
+  Todo.findById(req.params.id, function (err, todo) {
+
+    todo.content    = req.body.content;
+    todo.updated_at = Date.now();
+    todo.save(function (err, todo, count) {
+      if(err) return next(err);
+
+      res.redirect('/');
+    })
+  });
+};
+
 // ** express turns the cookie key to lowercase **
 exports.current_user = function (req, res, next) {
 
